57597cb6ba0e6033b3b0f904ff8c98397b97583e
Spencer Lazzar <spencerlazzar@yahoo.com>
2020-11-17 17:08:00 -0800

    Pre Deploy

--- resty/src/components/Form.js
+++ resty/src/components/Form.js
@@ -1,22 +1,31 @@
 import React, { Component } from 'react';
 
 class Form extends Component {
-  state = {
-    url: ''
+  constructor() {
+    super();
+    this.state = {
+      url: '',
+      method: ''
+    }
   }
 
   onChange = (e) => this.setState({ [e.target.name]: e.target.value });
 
-  onSubmit = (e) => {
+  handleClick = (e) => {
     e.preventDefault();
+    const method = e.target.value;
     this.props.Form(this.state.url);
-    this.setState({ url: '' });
+    this.setState({ method });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
   }
 
   render() {
     return (
       <div>
-        <form onSubmit={this.onSubmit}>
+        <form onSubmit={this.handleSubmit}>
           <label htmlFor="url">URL: </label>
           <input
             type="text"
@@ -31,12 +40,13 @@ class Form extends Component {
 
         </form>
         <br />
-        <button>GET</button>
-        <button>POST</button>
-        <button>PUT</button>
-        <button>DELETE</button>
+        <p>Method: {this.state.method}</p>
+        <button onClick={this.handleClick} value="GET">GET</button>
+        <button onClick={this.handleClick} value="POST">POST</button>
+        <button onClick={this.handleClick} value="PUT">PUT</button>
+        <button onClick={this.handleClick} value="DELETE">DELETE</button>
         <ul className='no-bull'>
-          <li>URL: {this.state.url} </li>
+          <li>URL: {this.state.url.value} </li>
         </ul>
       </div>
     );
